899dba2cda259525f7bbd8826396c153e4ddb74f
ds10x2 <padasol@khu.ac.kr>
2023-10-09 00:34:36 +0900

    Add MapActivity

--- Dashcam/app/src/main/java/com/example/dashcam/MapActivity.java
+++ Dashcam/app/src/main/java/com/example/dashcam/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.dashcam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
     }
 }
