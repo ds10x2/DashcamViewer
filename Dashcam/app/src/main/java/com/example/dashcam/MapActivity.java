3bf9451a2453ac78b0f984e5a1406e5bba6ec69b
ds10x2 <padasol@khu.ac.kr>
2023-11-09 00:48:33 +0900

    MapActivity 폴리곤 라인 그리기

--- Dashcam/app/src/main/java/com/example/dashcam/MapActivity.java
+++ Dashcam/app/src/main/java/com/example/dashcam/MapActivity.java
@@ -1,16 +1,74 @@
 package com.example.dashcam;
 
+import static android.icu.util.MeasureUnit.DOT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MapActivity extends AppCompatActivity
+    implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener{
+
+    private static final int PATTERN_GAP_LENGTH_PX = 20;
+    private static final PatternItem DOT = new Dot();
+    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+
+    private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap){
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(-35.016, 143.321),
+                        new LatLng(-34.747, 145.592),
+                        new LatLng(-34.364, 147.891),
+                        new LatLng(-33.501, 150.217),
+                        new LatLng(-32.306, 149.248),
+                        new LatLng(-32.491, 147.309)));
+
+        polyline1.setTag("A"); //다중선과 함께 데이터 객체(임의의 태그 A) 저장
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.684, 133.903), 4));
+
+        googleMap.setOnPolylineClickListener(this);
+        //googleMap.setOnPolygonClickListener(this);
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline){
+        if((polyline.getPattern() == null) || (!polyline.getPattern().contains(DOT))) {
+            polyline.setPattern(PATTERN_POLYLINE_DOTTED);
+        } else{
+            polyline.setPattern(null);
+        }
+        Toast.makeText(this, "Route type" + polyline.getTag().toString(), Toast.LENGTH_SHORT).show();
     }
 }
